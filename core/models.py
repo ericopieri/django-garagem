4ee42e975ad945cc29dbae595acb192798faa0f3
Erico Pieri <erico.pieri@gmail.com>
2022-08-27 02:03:42 -0300

    criado models Categoria e Marca

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.nome
+        
+
+class Marca(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
