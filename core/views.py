9e407642456cd10e145107c394024099c0462692
Erico Pieri <erico.pieri@gmail.com>
2022-08-27 02:21:03 -0300

    criado e instalado os endpoints  Marca e Categoria

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Marca, Categoria
+from core.serializers import MarcaSerializer, CategoriaSerializer
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
