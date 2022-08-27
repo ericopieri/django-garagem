b6e5db066c66634138e797be3f77f533cea3de6a
Erico Pieri <erico.pieri@gmail.com>
2022-08-27 02:36:47 -0300

    allowed apps "*" (all)

--- garagem/settings.py
+++ garagem/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = "django-insecure-@!ds^#ktmxgkrnguk%cj(a@mb6lro0al92viqc_a%k@*%0!$7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
